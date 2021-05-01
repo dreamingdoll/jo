b3ef3441e3dd91885a1b58a82aaaf4c9a2118e71
dreamingdoll <dreamingdoll.jo@gmail.com>
2021-05-01 16:35:44 +0900

    ㅂㅈㄷ

--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -35,27 +35,61 @@ print("naver/kakao/sk/samsung")
 print("first", end='');print("second")
 
 # 문제 10
-
+print(5/3)
 
 # 문제 11
+삼성전자 = 50000
+print(삼성전자 * 10)
 
 # 문제 12
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
 
 # 문제 13
+s = "hello"
+t = "python"
+print(s, end='!');print(t)
 
 # 문제 14
+"""
+[예상]
+2 + 2 * 3 = 8
+"""
+print(2 + 2 * 3)
 
 # 문제 15
+a = "132"
+print(type(a))
 
 # 문제 16
+num_str = "720"
+num_str = int(num_str)
+print(num_str)
+print(type(num_str))
 
 # 문제 17
+num = 100
+num = str(num)
+print(num)
+print(type(num))
 
 # 문제 18
+b = "15.79"
+b = float(b)
+print(b)
+print(type(b))
 
 # 문제 19
+year = "2020"
+year = int(year)
+print(year-3)
+print(year-2)
+print(year-1)
 
 # 문제 20
+aircon= 45854
+print(f"{aircon * 36}원")
 
 
 
