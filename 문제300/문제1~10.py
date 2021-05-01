421495fa810368b3d8f2671aaab1e8eac7365151
dreamingdoll <dreamingdoll.jo@gmail.com>
2021-05-01 16:18:09 +0900

    ㅂㅈㄷ

--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -1,7 +1,62 @@
 
-#문제1
+# 문제 1
+print("Hello World")
 
-asdasdasd
-asdasd
-assert async asd
+# 문제 2
+print("Mary's cosmetics")
+
+# 문제 3
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 문제 4
+print('"C:\Windows"')
+
+# 문제 5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+"""
+\n : 줄바꿈
+\t : Tab
+"""
+
+# 문제 6
+"""
+[예상]
+오늘은 일요일
+"""
+print("오늘은", "일요일")
+
+# 문제 7
+print("naver;kakao;sk;samsung")
+
+# 문제 8
+print("naver/kakao/sk/samsung")
+
+# 문제 9
+print("first", end='');print("second")
+
+# 문제 10
+
+
+# 문제 11
+
+# 문제 12
+
+# 문제 13
+
+# 문제 14
+
+# 문제 15
+
+# 문제 16
+
+# 문제 17
+
+# 문제 18
+
+# 문제 19
+
+# 문제 20
+
+
+
 
