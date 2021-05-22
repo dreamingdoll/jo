a24e8c28375b5a2034194cf1f3ac6023d619dd7c
dreamingdoll <dreamingdoll.jo@gmail.com>
2021-05-22 15:45:44 +0900

    ㅂㅈㄷ

---
+++ 문제300/문제211~220.py
@@ -0,0 +1,48 @@
+
+# 문제 211
+"""
+안녕
+Hi
+"""
+
+# 문제 212
+"""
+7
+15
+"""
+
+# 문제 213
+"""
+함수를 호출할때 문자열을 채우지 않아서
+"""
+
+# 문제 214
+"""
+a 와 b의 type 이 달라서
+"""
+
+# 문제 215
+def print_with_smile(a):
+    print(a+":D")
+
+# 문제 216
+print_with_smile("안녕하세요")
+
+# 문제 217
+def print_upper_price(a):
+    print(a*1.3)
+
+# 문제 218
+def print_sum(a, b):
+    print(a + b)
+
+# 문제 219
+def print_artimetic_operation(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+
+# 문제 220
+def print_max(a, b, c):
+    print(max(a, b, c))
